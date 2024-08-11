f98260fb5ca23f30dee2f8ecd8174280e843a496
Rafael <rafael271alves@gmail.com>
2024-08-11 18:39:23 -0300

    feat: resolvendo desafio

--- Program.cs
+++ Program.cs
@@ -13,13 +13,22 @@ hospedes.Add(p1);
 hospedes.Add(p2);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite1 = new Suite(tipoSuite: "Premium", capacidade: 3, valorDiaria: 30);
+Suite suite2 = new Suite(tipoSuite: "Basic", capacidade: 3, valorDiaria: 10);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+Reserva reserva1 = new Reserva(diasReservados: 10);
+reserva1.CadastrarSuite(suite1);
+reserva1.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+Console.WriteLine($"1° - Hóspedes: {reserva1.ObterQuantidadeHospedes()}");
+Console.WriteLine($"1° - Valor diária: {reserva1.CalcularValorDiaria()}");
+
+Reserva reserva2 = new Reserva(diasReservados: 5);
+reserva2.CadastrarSuite(suite2);
+reserva2.CadastrarHospedes(hospedes);
+
+// Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"2° - Hóspedes: {reserva2.ObterQuantidadeHospedes()}");
+Console.WriteLine($"2° - Valor diária: {reserva2.CalcularValorDiaria()}");
